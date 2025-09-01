1827ad878918698ec7c750700bd83965eb15a980
modeler :D <liam.beeson13@outlook.com>
2025-09-01 11:47:55 +1000

    Update main.js

--- meme_store/main.js
+++ meme_store/main.js
@@ -51,6 +51,10 @@ const raw_data = `[
     {
         "file": "memes/twerk.gif",
         "title": "Twerk"
+    },
+    {
+        "file": "memes/AAAAAAAAAAAAAAAA.jpg",
+        "title": "AAAAAAAAA"
     }
 ]`
 
